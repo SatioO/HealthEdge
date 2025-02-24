a7b1d7f0d8482516aa255dab7fa816ced667b37e
Vaibhav Satam <vaibhav.satam@accionlabs.com>
2025-02-25 00:20:27 +0530

    added provider meet link

--- services/appointment.tsx
+++ services/appointment.tsx
@@ -4,92 +4,112 @@ import { Provider } from '@/services/provider';
 import { getStoredAuthData } from '@/services/auth';
 
 export type AppointmentForm = {
-    category: string;
-    specialityId: number;
-    providerId: number;
-    slot: string;
+  category: string;
+  specialityId: number;
+  providerId: number;
+  slot: string;
 };
 
 export type CreateAppointmentOpenRequestDTO = {
-    dateTime: string;
-    specialityId: number;
-    providerId: number;
-    patientId?: number;
+  dateTime: string;
+  specialityId: number;
+  providerId: number;
+  patientId?: number;
 };
 
 export type SpecialityDTO = {
-    id: number;
-    name: string;
-    description: string;
-    category: string;
+  id: number;
+  name: string;
+  description: string;
+  category: string;
 };
 
 export type AppointmentResponseDTO = {
-    patient: any;
-    id: number;
-    dateTime: string; // ZonedDateTime represented as ISO string
-    speciality: SpecialityDTO;
-    provider: Provider;
+  patient: PatientDTO;
+  id: number;
+  dateTime: string; // ZonedDateTime represented as ISO string
+  speciality: SpecialityDTO;
+  provider: Provider;
+};
+
+export type PatientDTO = {
+  userId: number;
+  username: string;
+  name: {
+    familyName: string;
+    middleName: string | null;
+    givenName: string;
+  };
+  email: string;
+  dob: string;
+  roles: string[] | null;
+  administrativeSex: string;
+  maritalStatus: string;
+  status: string;
+  createdAt: string;
+  updatedAt: string;
+  addresses: any[];
+  mrn: string | null;
+  ssn: string | null;
+  licenseNumber: string | null;
+  is_deceased: string;
+  deceased_on: string | null;
 };
 
 const createAppointment = async (
-    params: CreateAppointmentOpenRequestDTO
+  params: CreateAppointmentOpenRequestDTO
 ): Promise<AppointmentResponseDTO> => {
-    try {
-        const auth = await getStoredAuthData();
-        params.patientId = auth?.user.userId;
-        const response = await fetch(`${BASE_URL}/appointments`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: 'Bearer ' + auth?.accessToken,
-            },
-            body: JSON.stringify(params),
-        });
+  try {
+    const auth = await getStoredAuthData();
+    params.patientId = auth?.user.userId;
+    const response = await fetch(`${BASE_URL}/appointments`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + auth?.accessToken,
+      },
+      body: JSON.stringify(params),
+    });
 
-        if (!response.ok) {
-            throw new Error(
-                `Appointment booking failed: ${response.statusText}`
-            );
-        }
-
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.error('Error during appointment booking:', error);
-        throw error;
+    if (!response.ok) {
+      throw new Error(`Appointment booking failed: ${response.statusText}`);
     }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error during appointment booking:', error);
+    throw error;
+  }
 };
 
 export const getAppointments = async (): Promise<AppointmentResponseDTO[]> => {
-    try {
-        const auth = await getStoredAuthData();
+  try {
+    const auth = await getStoredAuthData();
 
-        let url = '';
-        if (auth?.user.roles.includes('provider')) {
-            url = `${BASE_URL}/providers/${auth?.user.userId}/appointments?sort=dateTime`;
-        } else {
-            url = `${BASE_URL}/patients/${auth?.user.userId}/appointments?sort=dateTime`;
-        }
-        const response = await fetch(url, {
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: 'Bearer ' + auth?.accessToken,
-            },
-        });
-
-        if (!response.ok) {
-            throw new Error(
-                `Failed to fetch appointments: ${response.statusText}`
-            );
-        }
-
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.error('Error fetching appointments:', error);
-        throw error;
+    let url = '';
+    if (auth?.user.roles.includes('provider')) {
+      url = `${BASE_URL}/providers/${auth?.user.userId}/appointments?sort=dateTime`;
+    } else {
+      url = `${BASE_URL}/patients/${auth?.user.userId}/appointments?sort=dateTime`;
     }
+    const response = await fetch(url, {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + auth?.accessToken,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch appointments: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching appointments:', error);
+    throw error;
+  }
 };
 
 export { createAppointment };
